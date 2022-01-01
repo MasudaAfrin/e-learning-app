c63855350c8c0d7cfcb0eff023a41a3b4ca92460
MasudaAfrin <masudatuba@gmail.com>
2022-01-01 21:17:15 +0600

    admin side course create, update, user end course details

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,54 @@
+# rubocop:disable Style/Documentation
+
+class CoursesController < ApplicationController
+  before_action :course, only: %i[show update edit destroy]
+
+  def index
+    @courses = Course.order(created_at: :desc)
+  end
+
+  def show; end
+
+  def edit; end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    course = Course.new(course_params)
+    respond_to do |format|
+      if course.save
+        format.html { redirect_to courses_url, notice: 'Course is successfully created.' }
+      else
+        Rails.logger.error @course.errors.full_messages
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @course.update!(course_params)
+        format.html { redirect_to courses_url, notice: 'Course is successfully updated.' }
+      else
+        Rails.logger.error @course.errors.full_messages
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :subject)
+  end
+
+  def course
+    @course ||= Course.find_by(id: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: false
 
 Rails.application.routes.draw do
+  controller :pages do
+    get '/course/:slug', to: 'pages#course_details', as: 'course_details'
+  end
+  resources :courses
   root to: 'homepage#index', as: :homepage_index
 end
